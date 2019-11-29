969d278c7acdd02aeb93c18a6778086d32187225
tmontaigu <thomas.montaigu@laposte.net>
2019-11-29 21:11:58 +0100

    Typos

--- src/lib.rs
+++ src/lib.rs
@@ -117,18 +117,18 @@ impl Display for Error {
 impl std::error::Error for Error {
     fn description(&self) -> &str {
         match self {
-            Error::Message(ref msg) => {msg},
-            Error::IoError(_) => {"A std::io::Error occured"},
-            Error::ParseFloatError(_) => {"Failed to parse a float"},
-            Error::ParseIntError(_) => {"Failed to parse an int"},
-            Error::InvalidFieldType(_) => {"The field type is invalid"},
-            Error::InvalidDate => {"The date is invalid"},
-            Error::FieldNameTooLong => {"The Field name is too long to fit"},
-            Error::MissingMemoFile => {"A memo file was expected but could not be found"},
-            Error::ErrorOpeningMemoFile(_) => {"An error occured when trying to open the memo file"},
-            Error::BadConversion(_) => {"BadConvertion"},
-            Error::EndOfRecord => {"EndOfRecord"},
-            Error::MissingFields => {"Missing at least one field"},
+            Error::Message(ref msg) => { msg }
+            Error::IoError(_) => { "A std::io::Error occurred" }
+            Error::ParseFloatError(_) => { "Failed to parse a float" }
+            Error::ParseIntError(_) => { "Failed to parse an int" }
+            Error::InvalidFieldType(_) => { "The field type is invalid" }
+            Error::InvalidDate => { "The date is invalid" }
+            Error::FieldNameTooLong => { "The Field name is too long to fit" }
+            Error::MissingMemoFile => { "A memo file was expected but could not be found" }
+            Error::ErrorOpeningMemoFile(_) => { "An error occurred when trying to open the memo file" }
+            Error::BadConversion(_) => { "BadConversion" }
+            Error::EndOfRecord => { "EndOfRecord" }
+            Error::MissingFields => { "Missing at least one field" }
         }
     }
 }
